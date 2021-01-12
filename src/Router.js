fcadcb04b9ddfd5d875cf08ece19e410a8c3303e
bloodydark <justiceremain48694802@gmail.com>
2021-01-12 13:48:21 +0900

    roleで不具合発生

--- src/Router.js
+++ src/Router.js
@@ -9,6 +9,7 @@ import {
   ProductList,
   CartList,
   OrderConfirm,
+  OrderHistory,
 } from "./templates";
 import Auth from "./Auth";
 
@@ -26,6 +27,7 @@ const Router = () => {
 
         <Route exact path={"/cart"} component={CartList} />
         <Route exact path={"/order/confirm"} component={OrderConfirm} />
+        {/* <Route exact path={"/order/history"} component={OrderHistory} /> */}
       </Auth>
     </Switch>
   );
--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,4 +1,5 @@
 import {
+  // fetchOrdersHistoryAction,
   fetchProductsInCartAction,
   signInAction,
   signOutAction,
@@ -15,6 +16,26 @@ export const addProductToCart = (addedProduct) => {
     dispatch(push("/"));
   };
 };
+
+// export const fetchOrdersHistory = () => {
+//   return async (dispatch, getState) => {
+//     const uid = getState().users.uid;
+//     const list = [];
+
+//     db.collection("users")
+//       .doc(uid)
+//       .collection("orders")
+//       .orderBy("updated_at", "desc")
+//       .get()
+//       .then((snapshots) => {
+//         snapshots.forEach((snapshot) => {
+//           const data = snapshot.data();
+//           list.push(data);
+//         });
+//         dispatch(fetchOrdersHistoryAction(list));
+//       });
+//   };
+// };
 
 export const fetchProductsInCart = (products) => {
   return async (dispatch) => {
